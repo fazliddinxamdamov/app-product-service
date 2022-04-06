0eef10bd8e81e31cf3abe1fe9a25ca644b88a94d
fazliddin <fazliddinxamdamov102@gmail.com>
2022-04-06 16:34:57 +0500

    Bismillah

---
+++ src/test/java/com/fazliddin/appproductservice/AppProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazliddin.appproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
